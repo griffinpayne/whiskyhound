c948876fd3c291a8f1a70ff6a1808ab6d5b3d625
griffinpayne <griffin@griffin-payne.com>
2023-09-15 22:34:17 -0600

    optimized for speed

--- whiskyhound.py
+++ whiskyhound.py
@@ -1,63 +1,82 @@
 import re
-from bs4 import BeautifulSoup
+import csv
 import requests
-import time
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import threading
+from concurrent.futures import ThreadPoolExecutor
 
-def fetch_sitemap_urls(sitemap_url):
-    response = requests.get(sitemap_url)
-    soup = BeautifulSoup(response.content, 'xml')
-    urls = [url.text for url in soup.find_all('loc') if '/lot/' in url.text]
-    return urls
+session = requests.Session()
+
+def fetch_sitemap_urls(base_url, max_page):
+    all_urls = []
+    
+    def extract_urls(content):
+        soup = BeautifulSoup(content, 'xml')
+        return [url.text for url in soup.find_all('loc')]
+      
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(session.get, f"{base_url}?page={page}") for page in range(1, max_page+1)]
+        for future in futures:
+            content = future.result().content
+            urls = extract_urls(content)
+            all_urls.extend([url for url in urls if '/lot/' in url])
+            
+    return all_urls
 
 def fetch_winning_bid(url, brand_pattern, bottling_pattern):
     if not re.search(brand_pattern, url, re.IGNORECASE) or (bottling_pattern and not re.search(bottling_pattern, url, re.IGNORECASE)):
         return
     options = webdriver.FirefoxOptions()
     options.add_argument('-headless')
-    driver = webdriver.Firefox(options=options)
-    
-    try:
-        driver.get(url)
-        lot_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.uc-price'))  
-        )
-        for element in lot_element:
-            print(f"Winning bid for {url}: {element.text}")
-    except Exception as e:
-        print(f"No winning bid found for {url}. Exception: {e}")
-    finally:
-        driver.quit()
+    with webdriver.Firefox(options=options) as driver:
+        try:
+            driver.get(url)
+            lot_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#new-layout div.place-bid.bid-section.bid-info span'))
+            )
+            return (url, lot_element.text)
+        except Exception as e:
+            print(f"No winning bid found for {url}. Exception: {e}")
 
-if __name__ == '__main__':
+def prompt_to_output_to_csv():
+    csv_output = input("Do you want to output the results to a CSV file? (y/n) ")
+    if csv_output.lower() == "y":
+        csv_filename = input("Enter the name of the CSV file: ")
+        return csv_filename
+    else:
+        return None
+
+def main():
     brand = input("Enter the whisky brand you're interested in: ").replace('*', '.*')
     bottling = input("Enter the specific bottling (optional, press Enter to skip): ").replace('*', '.*')
     
-    sitemap_url = 'https://whiskyauctioneer.com/sitemap.xml?page=1'  # This skips over the first auction but there were issues with starting from the source
-    auction_urls = fetch_sitemap_urls(sitemap_url)
-    print(f"Fetched {len(auction_urls)} auction URLs. Filtering by brand: {brand}")
-
+    sitemap_url = 'https://whiskyauctioneer.com/sitemap.xml'
+    auction_urls = fetch_sitemap_urls(sitemap_url, 618)
     filtered_urls = [url for url in auction_urls if re.search(brand, url, re.IGNORECASE)]
     if bottling:
         filtered_urls = [url for url in filtered_urls if re.search(bottling, url, re.IGNORECASE)]
+    
     print(f"Filtered to {len(filtered_urls)} auction lot URLs.")
-
-    # Using threads to fetch winning bids
-    thread_list = []
-    for url in filtered_urls:
-        thread = threading.Thread(target=fetch_winning_bid, args=(url, brand, bottling))
-        thread.start()
-        thread_list.append(thread)
-        time.sleep(0.2)  # Introduce a small delay to avoid overwhelming the server
-
-    # Wait for all threads to complete
-    for thread in thread_list:
-        thread.join()
-
+    
+    results = []
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(fetch_winning_bid, url, brand, bottling) for url in filtered_urls]
+        for future in futures:
+            result = future.result()
+            if result:
+                results.append(result)
+                
     print("Scraping complete.")
+    
+    csv_filename = prompt_to_output_to_csv()
+    if csv_filename:
+        with open(csv_filename, "w", newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            for result in results:
+                csv_writer.writerow(result)
 
-# Need to test threading
+if __name__ == "__main__":
+    main()
